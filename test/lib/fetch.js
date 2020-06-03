8946c3b6d01c6678eccc8bf19fe4de0f2c4ed6c5
Kevin Malakoff <kmalakoff@gmail.com>
2020-06-03 07:37:14 -0700

    Check fetch

--- test/lib/fetch.js
+++ test/lib/fetch.js
@@ -18,24 +18,33 @@ module.exports = function fetch(endpoint, options, callback) {
       ? https.request({ host: parsed.host, path: parsed.path, port: 443, method: method, highWaterMark: options.highWaterMark })
       : http.request({ host: parsed.host, path: parsed.path, port: 80, method: method, highWaterMark: options.highWaterMark });
 
+  console.log(1, { host: parsed.host, path: parsed.path, port: 443, method: method, highWaterMark: options.highWaterMark });
+
   req.on('response', function response(res) {
+    console.log(2, res);
+
     // Follow 3xx redirects
     if (res.statusCode >= 300 && res.statusCode < 400 && 'location' in res.headers) {
+      console.log(3, res.statusCode);
       res.resume(); // Discard response
       return fetch(res.headers.location, callback);
     }
 
     // Only process if successful
     else if (res.statusCode < 200 || res.statusCode >= 300) {
+      console.log(4, res.statusCode);
       res.resume(); // Discard response
       return callback(new Error('Response code ' + res.statusCode + ' (' + http.STATUS_CODES[res.statusCode] + ')'));
     }
 
     // HEAD
     if (method === 'HEAD') {
+      console.log(4, res.statusCode);
       res.resume(); // Discard response
       return callback(null, res);
     }
+
+    console.log(5, res.statusCode);
 
     // GET
     var data = '';
