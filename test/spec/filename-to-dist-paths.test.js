773036a946ca4eade9edf1fb19e83583e302e0bb
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-10 19:31:05 -0800

    Upgrade to biomejs

--- test/spec/filename-to-dist-paths.test.js
+++ test/spec/filename-to-dist-paths.test.js
@@ -8,13 +8,13 @@ var distPaths = require('../..');
 function checkFileName(filename, version, callback) {
   var queue = new Queue(1);
   var all = distPaths(filename, version).reverse();
-  assert.ok(all.length > 0, filename + ' ' + version);
+  assert.ok(all.length > 0, `${filename} ${version}`);
 
   function next(callback) {
     if (!all.length) return callback();
     var distPath = all.pop();
-    log(filename + ' ' + distPath);
-    get('https://nodejs.org/dist/' + distPath).head(function (err, res) {
+    log(`${filename} ${distPath}`);
+    get(`https://nodejs.org/dist/${distPath}`).head((err, res) => {
       if (err) return callback(err);
       assert.equal(res.statusCode, 200, distPath);
       queue.defer(next);
@@ -32,7 +32,7 @@ function checkFiles(dist, callback) {
 
   function next(callback) {
     if (!all.length) return callback();
-    checkFileName(all.pop(), dist.version, function (err) {
+    checkFileName(all.pop(), dist.version, (err) => {
       if (err) return callback(err);
       queue.defer(next);
       callback();
@@ -47,17 +47,15 @@ var SPECIFIC_VERSION = null;
 var SKIP_TESTS = 0;
 var MAX_TESTS = 1;
 
-describe('filename-to-dist', function () {
+describe('filename-to-dist', () => {
   var dists = null;
 
-  before(function (done) {
-    get('https://nodejs.org/dist/index.json').json(function (err, res) {
+  before((done) => {
+    get('https://nodejs.org/dist/index.json').json((err, res) => {
       if (err) return done(err);
       dists = res.body;
       if (SPECIFIC_VERSION) {
-        dists = dists.filter(function (x) {
-          return x.version === SPECIFIC_VERSION;
-        });
+        dists = dists.filter((x) => x.version === SPECIFIC_VERSION);
       } else {
         if (SKIP_TESTS) dists = dists.slice(SKIP_TESTS);
         if (MAX_TESTS) dists = dists.slice(0, MAX_TESTS);
@@ -66,13 +64,13 @@ describe('filename-to-dist', function () {
     });
   });
 
-  it('all versions', function (done) {
+  it('all versions', (done) => {
     var queue = new Queue(1);
     var all = dists.slice().reverse();
 
     function next(callback) {
       if (!all.length) return callback();
-      checkFiles(all.pop(), function (err) {
+      checkFiles(all.pop(), (err) => {
         if (err) return callback(err);
         queue.defer(next);
         callback();
@@ -80,7 +78,7 @@ describe('filename-to-dist', function () {
     }
 
     queue.defer(next);
-    queue.await(function (err) {
+    queue.await((err) => {
       console.log('');
       done(err);
     });
