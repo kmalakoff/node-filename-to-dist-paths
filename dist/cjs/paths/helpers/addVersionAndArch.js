de933229825bd652a452ddc68e8a0455fd915c51
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-10 18:09:28 -0800

    Upgrade modules

--- dist/cjs/paths/helpers/addVersionAndArch.js
+++ dist/cjs/paths/helpers/addVersionAndArch.js
@@ -1,9 +1,9 @@
 "use strict";
 module.exports = function addVersionAndArch(key, version) {
-    var arch = key.split("/")[0];
-    var parts = key.split(".");
+    var arch = key.split('/')[0];
+    var parts = key.split('.');
     return [
         "".concat(parts[0], "-").concat(version, "-").concat(arch, ".").concat(parts[1])
     ];
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
