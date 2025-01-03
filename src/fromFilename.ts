19b1a8d72d348cc5d56e313043e6028ef8c802d6
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 02:38:21 -0800

    Update fromFilename to handle Windows zip extensions and string concat

--- src/fromFilename.ts
+++ src/fromFilename.ts
@@ -2,27 +2,24 @@ import type { Specifier } from './types';
 
 export default function fromFilename(filename: string, version: string, specifier: Specifier = {}) {
   const major = +version.split('.')[0].slice(1);
-
   const parts = filename.split('-');
   let platform = parts[0];
   const arch = parts[1];
+
   let ext = parts[2];
-  if (platform === 'osx') platform = 'darwin';
+  if (parts.length < 3) ext = platform === 'win' ? 'zip' : 'tar';
+  if (ext === 'tar') ext = 'tar.'.concat(specifier.compression || 'gz');
 
-  if (parts.length < 3) ext = 'tar';
-  if (ext === 'tar') ext = `tar.${specifier.compression || 'gz'}`;
-
-  if (platform === 'src') return `${version}/node-${version}.${ext}`;
-  if (platform === 'headers') return `${version}/SHASUMS256.txt`;
-  if (ext === 'pkg') return `${version}/node-${version}.${ext}`;
+  if (ext === 'pkg') return ''.concat(version, '/node-').concat(version, '.').concat(ext);
+  if (platform === 'src') return ''.concat(version, '/node-').concat(version, '.').concat(ext);
+  if (platform === 'headers') return ''.concat(version, '/SHASUMS256.txt');
 
   if (platform === 'win') {
     if (ext === 'exe') {
-      if (major === 0) return `${version}/node.${ext}`;
-      return `${version}/${platform}-${arch}/node.${ext}`;
+      if (major === 0) return ''.concat(version, '/node.').concat(ext);
+      return ''.concat(version, '/').concat(platform, '-').concat(arch, '/node.').concat(ext);
     }
-    if (ext === 'msi') return `${version}/node-${version}-${arch}.${ext}`;
-  }
-
-  return `${version}/node-${version}-${platform}-${arch}.${ext}`;
+    if (ext === 'msi') return ''.concat(version, '/node-').concat(version, '-').concat(arch, '.').concat(ext);
+  } else if (platform === 'osx') platform = 'darwin';
+  return ''.concat(version, '/node-').concat(version, '-').concat(platform, '-').concat(arch, '.').concat(ext);
 }
