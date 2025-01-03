74c59d70557db6642cc5f6fc017f98d6aab2fbf4
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-02 21:53:18 -0800

    Remove fromSpecifier and reduce dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export type * from './types';
 export * as constants from './constants';
 export { default } from './fromFilename';
-export { default as fromSpecifier } from './fromSpecifier';
 export { default as getDists } from './getDists';
