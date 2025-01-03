1c9cbd9c8ed4cc1509fcf50971abd8b1ab11d3ed
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-02 22:45:10 -0800

    Add metadata fields to Dist interface

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,15 @@
 export interface Dist {
   version: string;
+  date: string;
   files: string[];
+  npm: string;
+  v8: string;
+  uv: string;
+  zlib: string;
+  openssl: string;
+  modules: string;
+  lts: boolean;
+  security: boolean;
 }
 
 export interface Specifier {
