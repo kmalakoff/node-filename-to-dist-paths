e9b779b734e44b7b54bc9106c77a8698fc0a5651
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-02 22:09:27 -0800

    Export getDist from index file

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export type * from './types';
 export * as constants from './constants';
 export { default } from './fromFilename';
 export { default as getDists } from './getDists';
+export { default as getDist } from './getDist';
