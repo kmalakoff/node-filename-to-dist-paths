2e7c09a59de0d32da419c6e246babe2035ae191a
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-17 23:42:06 -0700

    Upgrade modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export type * from './types.js';
 export * as constants from './constants.js';
 export { default } from './fromFilename.js';
-export { default as getDists } from './getDists.js';
 export { default as getDist } from './getDist.js';
+export { default as getDists } from './getDists.js';
+export type * from './types.js';
