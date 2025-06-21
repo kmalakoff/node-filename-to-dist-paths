1ef556561020835d81eb7fc3ffa27d079ba78921
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 00:44:43 -0700

    Replace ts-ignore with dynamic require for constants import

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,8 @@
-// @ts-ignore
-import constants from '../../assets/constants.cjs';
+import Module from 'module';
 
-export const CACHE_PATH = constants.CACHE_PATH;
-export const DISTS_URL = constants.DISTS_URL;
+const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
+const path = '../../assets/constants.cjs';
+const constants = _require(path);
+
+export const CACHE_PATH = constants.CACHE_PATH as string;
+export const DISTS_URL = constants.DISTS_URL as string;
