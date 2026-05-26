3f0b3e7fd0084945ccf26498b5af2f760100a893
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 08:56:16 -0700

    Fix broken types

--- src/getDist.ts
+++ src/getDist.ts
@@ -4,8 +4,9 @@ import type { Dist } from './types.ts';
 
 export default function getDist(version: string): Dist | null {
   const dists = getDists();
-  for (const key in dists) {
-    if (dists[key].version === version) return dists[key];
+  if (!dists) return null;
+  for (const dist of dists) {
+    if (dist.version === version) return dist;
   }
   return null;
 }
