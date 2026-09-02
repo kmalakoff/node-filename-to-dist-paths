3d4f66b4763cad80b65b270e640da6d283e8d7d9
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-01 23:33:41 -0700

    Refactor module to use match-semver and optimize memory usage

--- test/spec/filename-to-dist-paths.test.js
+++ test/spec/filename-to-dist-paths.test.js
@@ -1,65 +1,84 @@
 var assert = require('assert');
 var log = require('single-line-log').stdout;
 var Queue = require('queue-cb');
-var superagent = require('superagent');
+var get = require('get-remote');
 
 var distPaths = require('../..');
 
-function checkExists(distPath, filename, callback) {
-  log(filename, distPath);
-  superagent.head('https://nodejs.org/dist/' + distPath).end(function (err, res) {
-    if (err) return callback(err);
-    assert.equal(res.statusCode, 200, distPath);
-    callback();
-  });
-}
-
 function checkFileName(filename, version, callback) {
-  var paths = distPaths(filename, version);
-  assert.ok(distPaths.length > 0, filename + ' ' + version);
-
   var queue = new Queue(1);
-  for (var index = 0; index < paths.length; index++) {
-    queue.defer(checkExists.bind(null, paths[index], filename));
+  var all = distPaths(filename, version).reverse();
+  assert.ok(all.length > 0, filename + ' ' + version);
+
+  function next(callback) {
+    if (!all.length) return callback();
+    var distPath = all.pop();
+    log(filename, distPath);
+    get('https://nodejs.org/dist/' + distPath).head(function (err, res) {
+      if (err) return callback(err);
+      assert.equal(res.statusCode, 200, distPath);
+      queue.defer(next);
+      callback();
+    });
   }
+
+  queue.defer(next);
   queue.await(callback);
 }
 
 function checkFiles(dist, callback) {
   var queue = new Queue(1);
-  for (var index = 0; index < dist.files.length; index++) {
-    queue.defer(checkFileName.bind(null, dist.files[index], dist.version));
+  var all = dist.files.slice().reverse();
+
+  function next(callback) {
+    if (!all.length) return callback();
+    checkFileName(all.pop(), dist.version, function (err) {
+      if (err) return callback(err);
+      queue.defer(next);
+      callback();
+    });
   }
+
+  queue.defer(next);
   queue.await(callback);
 }
 
-var SPECIFIC_VERSION = null; // 'v0.12.18'
+var SPECIFIC_VERSION = null;
 var MAX_TESTS = 1;
 
 describe('filename-to-dist', function () {
   var dists = null;
 
   before(function (done) {
-    superagent
-      .get('https://nodejs.org/dist/index.json')
-      .set('Accept', 'application/json')
-      .end(function (err, res) {
-        if (err) return done(err);
-        dists = res.body;
-        if (SPECIFIC_VERSION) {
-          dists = dists.filter(function (x) {
-            return x.version === SPECIFIC_VERSION;
-          });
-        } else if (MAX_TESTS) dists = dists.slice(0, MAX_TESTS);
-        done(err);
-      });
+    get('https://nodejs.org/dist/index.json').json(function (err, res) {
+      if (err) return done(err);
+      dists = res.body;
+      if (SPECIFIC_VERSION) {
+        dists = dists.filter(function (x) {
+          return x.version === SPECIFIC_VERSION;
+        });
+      } else if (MAX_TESTS) dists = dists.slice(0, MAX_TESTS);
+      done(err);
+    });
   });
 
   it('all versions', function (done) {
     var queue = new Queue(1);
-    for (var index = 0; index < dists.length; index++) {
-      queue.defer(checkFiles.bind(null, dists[index]));
+    var all = dists.slice().reverse();
+
+    function next(callback) {
+      if (!all.length) return callback();
+      checkFiles(all.pop(), function (err) {
+        if (err) return callback(err);
+        queue.defer(next);
+        callback();
+      });
     }
-    queue.await(done);
+
+    queue.defer(next);
+    queue.await(function (err) {
+      console.log('');
+      done(err);
+    });
   });
 });
