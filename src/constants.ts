1c4232ad8152952ea0bd62f441a715421653337e
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-01 23:44:25 -0700

    Simplify constant loading and upgrade dependencies

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,7 @@
 import Module from 'module';
 
 const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
-const path = '../../assets/constants.cjs';
-const constants = _require(path);
+const constants = _require('../../assets/constants.cjs');
 
 export const CACHE_PATH = constants.CACHE_PATH as string;
 export const DISTS_URL = constants.DISTS_URL as string;
