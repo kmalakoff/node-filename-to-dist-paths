0d201fe8cc4da1133bc5e1d489efadc6f867eafb
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-02 19:49:49 -0700

    Upgrade modules

---
+++ test/exports/import.test.ts
@@ -0,0 +1,23 @@
+import assert from 'assert';
+import fromFilename, { constants, getDist, getDistAsync, getDists, getDistsAsync } from 'node-filename-to-dist-paths';
+
+describe('exports .ts', () => {
+  it('default', () => {
+    assert.equal(typeof fromFilename, 'function');
+  });
+  it('constants', () => {
+    assert.equal(typeof constants, 'object');
+  });
+  it('getDist', () => {
+    assert.equal(typeof getDist, 'function');
+  });
+  it('getDistAsync', () => {
+    assert.equal(typeof getDistAsync, 'function');
+  });
+  it('getDists', () => {
+    assert.equal(typeof getDists, 'function');
+  });
+  it('getDistsAsync', () => {
+    assert.equal(typeof getDistsAsync, 'function');
+  });
+});
